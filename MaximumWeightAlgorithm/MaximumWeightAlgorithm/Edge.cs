3db84712da6006bf881d0f2cf9daf639fd7b2a48
Cees-Jan <ceesjannolen@live.nl>
2016-09-30 15:28:55 +0200

    started over. Added many declarations. Now going to the actual algorithm

--- MaximumWeightAlgorithm/MaximumWeightAlgorithm/Edge.cs
+++ MaximumWeightAlgorithm/MaximumWeightAlgorithm/Edge.cs
@@ -1,18 +1,46 @@
-﻿namespace MaximumWeightAlgorithm
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters;
+
+namespace MaximumWeightAlgorithm
 {
     public class Edge
     {
-        public Edge(int start, int end, double weight)
+        private List<int> _elems;
+
+        public Edge(int start, int end, int weight)
         {
-            Start = start;
-            End = end;
-            Weight = weight;
+            _elems = new List<int>();
+            if (start == end)
+            {
+                _elems.Insert(0, start);
+                _elems.Insert(1, end);
+                _elems.Insert(2, weight);
+            }
+            if (start < end)
+            {
+                _elems.Insert(0, start);
+                _elems.Insert(1, end);
+            }
+            else
+            {
+                _elems.Insert(0, end);
+                _elems.Insert(1, start);
+            }
+            _elems.Insert(2, weight);
+
         }
 
-        public int Start { get; set; }
+        public int this [int i] => _elems[i];
 
-        public double Weight { get; set; }
+        public override string ToString()
+        {
+            var result = "";
+            result += _elems[0] + "\t" + _elems[1] + "\t" + _elems[2] + "\n ";
+            return result;
+        }
 
-        public int End { get; set; }
+        //equivalent (start , end)
+        //equivalent (start, end , weight)
     }
 }
--- MaximumWeightAlgorithm/MaximumWeightAlgorithm/MaxWeightMatching.cs
+++ MaximumWeightAlgorithm/MaximumWeightAlgorithm/MaxWeightMatching.cs
@@ -7,53 +7,156 @@ namespace MaximumWeightAlgorithm
 {
     public class MaxWeightMatching
     {
-        public static void MaxWMatching(List<Edge>  edges )
+        private static int _amountOfEdges;
+        private static int _amountOfNodes;
+        private static int _maxWeight;
+        private static List<int> _endpoint = new List<int>();
+        private static List<List<int>> _neightbend = new List<List<int>>();
+        private static List<int> _mate = new List<int>();
+        private static List<int> _label = new List<int>();
+        private static List<int> _labelend;
+        private static List<int> _inblossom;
+        private static List<int> _blossomparent;
+        private static List<List<int>> _blossomchilds;
+        private static List<int> _blossombase;
+        private static List<List<int>> _blossomdps;
+        private static List<int> _bestedge;
+        private static List<List<int>> _blossombestedges;
+        private static List<int> _unusedblossoms;
+        private static List<int> _dualvar;
+        private static List<bool> _allowedge;
+        private static List<int> _queue;
+
+        public static void MaxWMatching(List<Edge> edges)
         {
-
-
             // if edges is empty
-            if (edges.Count<= 0)
+            if (edges.Count <= 0)
                 return;
 
             // Count nodes
-            var edgesLenght = edges.Count;
-            var nodesLenght = 0;
+            _amountOfEdges = edges.Count;
+            _amountOfNodes = 0;
+
             foreach (var edge in edges)
             {
-                if (edge.Start >= nodesLenght)
-                    nodesLenght = edge.Start +1;
-                if (edge.End >= nodesLenght)
-                    nodesLenght = edge.End + 1;
+                if (!(edge[0] >= 0 && edge[1] >= 0 && edge[0] != edge[1]))
+                    throw new Exception("Assert Error");
+                if (edge[0] >= _amountOfNodes) _amountOfNodes = edge[0] + 1;
+                if (edge[1] >= _amountOfNodes) _amountOfNodes = edge[1] + 1;
             }
+            Console.WriteLine("Applying Edmonds Algorithm with {0} nodes transformed by Siloach reducer\n",
+                _amountOfNodes);
 
-            // Find maximum weight
-            var maxWeight = Math.Max(0, edges.Max(edge => edge.Weight));
-
-            //
-            var endpoints = new List<int>();
-            for (var i = 0; i < edgesLenght; i++)
+            for (var i = 0; i < _amountOfEdges; i++)
             {
-                endpoints.Add(edges[i].Start);
-                endpoints.Add(edges[i].End);
+                _maxWeight = Math.Max(0, Math.Max(_maxWeight, edges[i][2]));
             }
 
-            foreach (var VARIABLE in endpoints)
+            for (var i = 0; i < 2 * _amountOfEdges; i++)
             {
-                Console.Write(VARIABLE + ", ");
+                _endpoint.Add(edges[DivideAndFloor(i, 2)][i % 2]);
             }
-            Console.WriteLine();
 
+            for (var i = 0; i < _amountOfNodes; i++)
+            {
+                _neightbend.Add(new List<int>());
+            }
+
+            for (var k = 0; k < _amountOfEdges; k++)
+            {
+                var i = edges[k][0];
+                var j = edges[k][1];
+
+                _neightbend[i].Add(2 * k + 1);
+                _neightbend[j].Add(2 * k);
+            }
+
+            for (var i = 0; i < _amountOfNodes; i++)
+            {
+                _mate.Add(-1);
+                _inblossom.Add(i);
+                _blossombase.Add(i);
+                _bestedge.Add(-1);
+                _blossomdps.Add(new List<int>());
+                _dualvar.Add(_maxWeight);
+            }
+
+            for (var i = 0; i < 2 * _amountOfNodes; i++)
+            {
+                _label.Add(0);
+                _labelend.Add(-1);
+                _blossomparent.Add(-1);
+                _blossomchilds.Add(new List<int>());
+                _blossombestedges.Add(new List<int>());
+            }
+            for (var i = 0; i < _amountOfNodes; i++)
+            {
+                _blossombase.Add(-1);
+                _dualvar.Add(0);
+            }
+            for (var i = _amountOfNodes; i < 2 * _amountOfNodes; i++)
+            {
+                _unusedblossoms.Add(i);
+            }
+            for (var i = 0; i < _amountOfEdges; i++)
+            {
+                _allowedge.Add(false);
+            }
+
+            
+
+
+//            foreach (var edge in edges)
+//            {
+//                if (edge.Start >= nodesLenght)
+//                    nodesLenght = edge.Start + 1;
+//                if (edge.End >= nodesLenght)
+//                    nodesLenght = edge.End + 1;
+//            }
 //
+//            // Find maximum weight
+//            var maxWeight = Math.Max(0, edges.Max(edge => edge.Weight));
+//
+//            //
+//            var endpoints = new List<int>();
+//            for (var i = 0; i < _amountOfEdges; i++)
+//            {
+//                endpoints.Add(edges[i].Start);
+//                endpoints.Add(edges[i].End);
+//            }
+//
+
+
 //            // Neighbend[Node] is the list of nodes of the edges to node
-//            var neighbend = new
-//            for (var i = 0; i < edgesLenght; i++)
+//            Console.WriteLine(nodesLenght);
+//            var neighbend = CreateList<List<int>>(nodesLenght);
+//            for (var i = 0; i < neighbend.Count; i++)
+//                neighbend[i] = new List<int>();
+//
+//            for (var i = 0; i < _amountOfEdges; i++)
 //            {
 //                var edge = edges[i];
-//
+//                neighbend[edge.Start].Add(2 * i + 1);
+//                neighbend[edge.End].Add(2 * i);
 //            }
+//
+//            // mate[v] is the remote endpoint of its matched edge, or -1 if it is single
+//            // (i.e. endpoint[mate[v]] is v's partner vertex).
+//            var mate = new List<int>();
+//            for (var i = 0; i < nodesLenght; i++)
+//            {
+//                mate.Add(-1);
+//            }
+        }
 
+        private static int DivideAndFloor(int i, int j)
+        {
+            return (int) Math.Floor((float) i / j);
+        }
 
-
+        private static List<T> CreateList<T>(int capacity)
+        {
+                return Enumerable.Repeat(default(T), capacity).ToList();
         }
     }
 }
